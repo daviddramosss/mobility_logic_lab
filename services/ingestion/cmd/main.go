cc2dcd5c1e81ba947fc2528771857df9e0933cc7
David Ramos <davidramosdelucas@gmail.com>
2026-04-01 12:54:21 +0200

    0.0.2 - "Main.go y go.mod"

--- services/ingestion/cmd/main.go
+++ services/ingestion/cmd/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"      //Para imprimir por consola
+	"log"      //Para manejar errores
+	"net/http" //Para manejar peticiones HTTP
+)
 
 func main() {
-	fmt.Println("Hello World")
+	//Definimos el puerto en el que va a escuchar el servidor
+	port := ":8080"
+
+	// 2. Configuramos las rutas (endpoints)
+	// Definimos un "healthcheck" básico para comprobar que el servidor está vivo.
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status": "ok", "service": "ingestion"}`))
+	})
+
+	//3. Mensaje para dar feedback
+	fmt.Printf("Ingestion Service arrancando en el puerto %s...\n", port)
+
+	//4. Inicio el servidor HTTP
+	// ListenAndServe bloquea el hilo principal y se queda escuchando peticiones de red
+	//Abre el puerto 8080 y se queda escuchando infinitamente en un bucle esperando peticiones.
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatalf("Error al iniciar el servidor: %v", err)
+	}
 }
---
+++ services/ingestion/go.mod
@@ -0,0 +1,3 @@
+module mobility_logic_lab/ingestion
+
+go 1.21
